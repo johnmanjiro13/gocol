4bca6115c0a78c64a6e487534b6dd71b1cce4734
johnmanjiro13 <28798279+johnmanjiro13@users.noreply.github.com>
2022-04-15 23:16:01 +0900

    feat: add All, Any, Index, Include and Filter

--- slices/slices.go
+++ slices/slices.go
@@ -1,9 +1,50 @@
 package slices
 
+func All[E any](s []E, f func(E) bool) bool {
+	for _, v := range s {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+func Any[E any](s []E, f func(E) bool) bool {
+	for _, v := range s {
+		if f(v) {
+			return true
+		}
+	}
+	return false
+}
+
+func Index[E comparable](s []E, v E) int {
+	for i, vs := range s {
+		if vs == v {
+			return i
+		}
+	}
+	return -1
+}
+
+func Include[E comparable](s []E, v E) bool {
+	return Index(s, v) >= 0
+}
+
 func Map[S1 ~[]E1, S2 []E2, E1, E2 any](s S1, f func(E1) E2) S2 {
 	dst := make(S2, len(s), len(s))
 	for i, v := range s {
 		dst[i] = f(v)
+	}
+	return dst
+}
+
+func Filter[S ~[]E, E any](s S, f func(E) bool) S {
+	dst := make(S, 0)
+	for _, v := range s {
+		if f(v) {
+			dst = append(dst, v)
+		}
 	}
 	return dst
 }
--- slices/slices_test.go
+++ slices/slices_test.go
@@ -2,12 +2,85 @@ package slices_test
 
 import (
 	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/magiconair/properties/assert"
 
 	"johnmanjiro13/gocol/slices"
 )
+
+func isEven(i int) bool {
+	return i%2 == 0
+}
+
+func TestAll(t *testing.T) {
+	tests := map[string]struct {
+		s    []int
+		f    func(int) bool
+		want bool
+	}{
+		"nil slice":    {nil, isEven, true},
+		"blank slice":  {nil, isEven, true},
+		"all even":     {[]int{2, 4, 6, 8}, isEven, true},
+		"contains odd": {[]int{1, 2, 4, 6, 8}, isEven, false},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, tt.want, slices.All(tt.s, tt.f))
+		})
+	}
+}
+
+func TestAny(t *testing.T) {
+	tests := map[string]struct {
+		s    []int
+		f    func(int) bool
+		want bool
+	}{
+		"nil slice":    {nil, isEven, false},
+		"blank slice":  {nil, isEven, false},
+		"all even":     {[]int{2, 4, 6, 8}, isEven, true},
+		"contains odd": {[]int{1, 2, 4, 6, 8}, isEven, true},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, tt.want, slices.Any(tt.s, tt.f))
+		})
+	}
+}
+
+var indexTests = map[string]struct {
+	s    []int
+	v    int
+	want int
+}{
+	"nil slice":        {nil, 0, -1},
+	"blank slice":      {[]int{}, 0, -1},
+	"single value":     {[]int{1, 2, 3}, 2, 1},
+	"duplicate value":  {[]int{1, 2, 2, 3}, 2, 1},
+	"duplicate value2": {[]int{1, 2, 3, 2}, 2, 1},
+	"not existed":      {[]int{1, 2, 3}, 4, -1},
+}
+
+func TestIndex(t *testing.T) {
+	for name, tt := range indexTests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, tt.want, slices.Index(tt.s, tt.v))
+		})
+	}
+}
+
+func TestInclude(t *testing.T) {
+	for name, tt := range indexTests {
+		t.Run(name, func(t *testing.T) {
+			got := slices.Include(tt.s, tt.v)
+			assert.Equal(t, tt.want != -1, got)
+		})
+	}
+}
 
 func TestMap(t *testing.T) {
 	t.Run("double integers", func(t *testing.T) {
@@ -27,5 +100,26 @@ func TestMap(t *testing.T) {
 		s := intSlice{1, 2, 3, 4, 5}
 		got := slices.Map(s, func(i int) int { return i * 2 })
 		assert.Equal(t, []int{2, 4, 6, 8, 10}, got)
+	})
+}
+
+func TestFilter(t *testing.T) {
+	t.Run("int", func(t *testing.T) {
+		s := []int{1, 2, 3, 4, 5}
+		got := slices.Filter(s, isEven)
+		assert.Equal(t, []int{2, 4}, got)
+	})
+
+	t.Run("string", func(t *testing.T) {
+		s := []string{"apple", "banana", "avocado"}
+		got := slices.Filter(s, func(s string) bool { return strings.HasPrefix(s, "a") })
+		assert.Equal(t, []string{"apple", "avocado"}, got)
+	})
+
+	t.Run("original type", func(t *testing.T) {
+		type intSlice []int
+		s := intSlice{1, 2, 3, 4, 5}
+		got := slices.Filter(s, isEven)
+		assert.Equal(t, intSlice{2, 4}, got)
 	})
 }
